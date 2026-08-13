093e496b096b88daf853545ab2c8ac3a9b8b3d93
collins-1111 <collins2tall@gmail.com>
2026-08-14 00:52:05 +0100

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Providers } from "@/components/providers";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@uploadthing/react/styles.css";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Hostel Allocation & Complaint System",
+  description: "Advanced university hostel management system.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
